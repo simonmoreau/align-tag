2418e283ed3c2ba83d5c3ff92ceaf72edae4eef3
Simon Moreau <simon@bim42.com>
2015-06-21 01:12:23 +0200

    Refactor the TagLeader class Run calculation in view coordinates

--- AlignTag/Arrange.cs
+++ AlignTag/Arrange.cs
@@ -73,88 +73,58 @@ namespace AlignTag
             Document doc = uidoc.Document;
             View activeView = doc.ActiveView;
 
-            ////Retrive all tag in active view
-            //List<IndependentTag> tags =
-            //    new FilteredElementCollector(doc, activeView.Id).
-            //    OfClass(typeof(IndependentTag)).WhereElementIsNotElementType().ToElements()
-            //    .ToList().Cast<IndependentTag>().ToList();
+            //Check the current view
+            if (!activeView.CropBoxActive)
+            {
+                throw new ErrorMessageException("Please set a crop box to the view");
+            }
 
             IEnumerable<IndependentTag> tags = from elem in new FilteredElementCollector(doc, activeView.Id).OfClass(typeof(IndependentTag)).WhereElementIsNotElementType()
                                                let type = elem as IndependentTag
                                                where type.HasLeader == true
                                                select type;
 
-            tx.Start("Arrange Tags");
+            tx.Start("Prepare Tags");
 
-            //Create a list of location points for tag headers
-            List<XYZ> headersPoints = CreateTagPositionPoints(activeView, tags, tx);
-
-            //Create a list of TagLeader to sort them
-            List<TagLeader> tagLeaders = new List<TagLeader>();
-
+            //Remove all leader to find the correct tag height and width
             foreach (IndependentTag tag in tags)
             {
-                if (tag.HasLeader == true)
-                {
-                    TagLeader tagLeader = new TagLeader(tag, doc);
-                    tagLeaders.Add(tagLeader);
-
-                    //Find nearest point
-                    XYZ nearestPoint = FindNearestPoint(headersPoints, tagLeader.LeaderStart);
-                    tagLeader.Tag.TagHeadPosition = nearestPoint;
-
-                    //remove this point from the list
-                    headersPoints.Remove(nearestPoint);
-                }
+                tag.LeaderEndCondition = LeaderEndCondition.Free;
+                tag.LeaderEnd = tag.TagHeadPosition;
             }
 
-            SortTag(tagLeaders);
-            CreateElbow(tagLeaders, activeView);
+            tx.Commit();
+            tx.Start("Arrange Tags");
 
+            //Create a list of TagLeader
+            List<TagLeader> tagLeaders = new List<TagLeader>();
+            foreach (IndependentTag tag in tags)
+            {
+                tagLeaders.Add(new TagLeader(tag, doc));
+            }
+
+            //Create a list of location points for tag headers
+            List<XYZ> tagHeadPoints = CreateTagPositionPoints(activeView,tagLeaders);
+
+            //place TagLeader
+            foreach (TagLeader tag in tagLeaders)
+            {
+                //Find nearest point
+                XYZ nearestPoint = FindNearestPoint(tagHeadPoints, tag.TagHeadPosition);
+                tag.TagHeadPosition = nearestPoint;
+
+                //remove this point from the list
+                tagHeadPoints.Remove(nearestPoint);
+            }
+
+            //replace all leaders before commiting
+            foreach (TagLeader tag in tagLeaders)
+            {
+                tag.UpdateTagPosition();
+            }
 
             tx.Commit();
 
-        }
-
-        private void CreateElbow(List<TagLeader> tagLeaders, View activeView)
-        {
-            BoundingBoxXYZ bbox = activeView.CropBox;
-            Transform viewTransform = bbox.Transform;
-
-            foreach (TagLeader tagLeader in tagLeaders)
-            {
-                XYZ A = viewTransform.Inverse.OfPoint(tagLeader.Tag.TagHeadPosition);
-                XYZ B = viewTransform.Inverse.OfPoint(tagLeader.LeaderStart);
-                XYZ AB = B - A;
-                double mult = AB.X * AB.Y;
-                mult = mult / Math.Abs(mult);
-                XYZ delta = new XYZ(AB.X - AB.Y * Math.Tan(mult * Math.PI / 4), 0, 0);
-                XYZ elbowPosition = A + delta;
-                elbowPosition = viewTransform.OfPoint(elbowPosition);
-
-                tagLeader.Tag.LeaderElbow = elbowPosition;
-            }
-        }
-
-        private void SortTag(List<TagLeader> tagLeaders)
-        {
-            foreach (TagLeader tagLeader in tagLeaders)
-            {
-                foreach (TagLeader secondTagLeader in tagLeaders)
-                {
-                    if (secondTagLeader != tagLeader)
-                    {
-                        //Check if leader cross
-                        if (tagLeader.Leader.Intersect(secondTagLeader.Leader) == SetComparisonResult.Overlap)
-                        {
-                            //Invert two tags
-                            XYZ leaderEnd = tagLeader.Tag.TagHeadPosition;
-                            tagLeader.Tag.TagHeadPosition = secondTagLeader.Tag.TagHeadPosition;
-                            secondTagLeader.Tag.TagHeadPosition = leaderEnd;
-                        }
-                    }
-                }
-            }
         }
 
         private XYZ FindNearestPoint(List<XYZ> points, XYZ basePoint)
@@ -175,64 +145,13 @@ namespace AlignTag
             return nearestPoint;
         }
 
-
-
-        private List<XYZ> CreateTagPositionPoints(View activeView, IEnumerable<IndependentTag> tags, Transaction tx)
+        private List<XYZ> CreateTagPositionPoints(View activeView, List<TagLeader> tagLeaders)
         {
-            if (!activeView.CropBoxActive)
-            {
-                throw new ErrorMessageException("Please set a crop box to the view");
-            }
-
-            ////Remove all leader
-            foreach (IndependentTag tag in tags)
-            {
-                tag.LeaderEndCondition = LeaderEndCondition.Free;
-                tag.LeaderEnd = tag.TagHeadPosition;
-                //tag.HasLeader = false;
-            }
-
-            tx.Commit();
-            tx.Start("Follow up");
-
             BoundingBoxXYZ bbox = activeView.CropBox;
-            XYZ origin = activeView.Origin;
-            Transform viewTransform = bbox.Transform;
-
-
 
             //Get largest tag dimension
-            double tagHeight = 0;
-            double tagWidth = 0;
-            foreach (IndependentTag tag in tags)
-            {
-
-                BoundingBoxXYZ tagBox = tag.get_BoundingBox(activeView);
-                BoundingBoxXYZ transformedTagBox = new BoundingBoxXYZ();
-                transformedTagBox.Min = viewTransform.Inverse.OfPoint(tagBox.Min);
-                transformedTagBox.Max = viewTransform.Inverse.OfPoint(tagBox.Max);
-
-                //get largest width
-                if (Math.Abs(transformedTagBox.Max.X - transformedTagBox.Min.X) > tagWidth)
-                {
-                    tagWidth = Math.Abs(transformedTagBox.Max.X - transformedTagBox.Min.X);
-                }
-
-                //get largest height
-                if (Math.Abs(transformedTagBox.Max.Y - transformedTagBox.Min.Y) > tagHeight)
-                {
-                    tagHeight = Math.Abs(transformedTagBox.Max.Y - transformedTagBox.Min.Y);
-                }
-
-            }
-
-            //replace all leader
-            foreach (IndependentTag tag in tags)
-            {
-                //tag.HasLeader = true;
-                tag.LeaderEndCondition = LeaderEndCondition.Attached;
-            }
-
+            double tagHeight = tagLeaders.Max(x => x.TagHeight);
+            double tagWidth = tagLeaders.Max(x => x.TagWidth);
 
             List<XYZ> points = new List<XYZ>();
             double step = tagHeight * 1.5;
@@ -243,14 +162,13 @@ namespace AlignTag
             for (int i = 0; i < max; i++)
             {
                 //Add left point
-                points.Add(viewTransform.OfPoint(new XYZ(bbox.Min.X, step * i, 0)));
+                points.Add(new XYZ(bbox.Min.X, step * i, 0));
 
                 //Add right point
-                points.Add(viewTransform.OfPoint(new XYZ(bbox.Max.X, step * i, 0)));
+                points.Add(new XYZ(bbox.Max.X, step * i, 0));
             }
 
             return points;
-
         }
     }
 
@@ -258,78 +176,95 @@ namespace AlignTag
     {
         private Document _doc;
         private View _currentView;
+        private Element _taggedElement;
+        private IndependentTag _tag;
 
         public TagLeader(IndependentTag tag, Document doc)
         {
             _doc = doc;
             _currentView = _doc.GetElement(tag.OwnerViewId) as View;
             _tag = tag;
-            GetTaggedElement();
-            GetLeaderStart();
 
+            _taggedElement = GetTaggedElement();
+            _tagHeadPosition = _currentView.CropBox.Transform.Inverse.OfPoint(tag.TagHeadPosition);
+            _tagHeadPosition = new XYZ(_tagHeadPosition.X, _tagHeadPosition.Y, 0);
+            _leaderEnd = GetLeaderEnd();
+            GetTagDimension();
         }
 
-        private Element _taggedElement;
-        public Element TaggedElement
+        private XYZ _tagHeadPosition;
+        public XYZ TagHeadPosition
         {
-            get { return _taggedElement; }
-        }
-
-        private IndependentTag _tag;
-        public IndependentTag Tag
-        {
-            get { return _tag; }
-        }
-
-        private Line _leader;
-        public Line Leader
-        {
-            get
+            get { return _tagHeadPosition; }
+            set 
             {
-                _leader = Line.CreateBound(_leaderStart, _tag.TagHeadPosition);
-                return _leader;
+                _tagHeadPosition = value; 
+                UpdateElbowPosition();
             }
         }
 
-        private XYZ _elbow;
-        public XYZ Elbow
+        private XYZ _elbowPosition;
+        public XYZ ElbowPosition
         {
-            get
-            {
-                Transform viewTransform = _currentView.CropBox.Transform;
-                XYZ A = viewTransform.Inverse.OfPoint(_tag.TagHeadPosition);
-                XYZ B = viewTransform.Inverse.OfPoint(_leaderStart);
-                XYZ AB = B - A;
+            get { return _elbowPosition; }
+        }
+
+        private void UpdateElbowPosition()
+        {
+                //Update elbow position
+                XYZ AB = _leaderEnd - _tagHeadPosition;
                 double mult = AB.X * AB.Y;
                 mult = mult / Math.Abs(mult);
                 XYZ delta = new XYZ(AB.X - AB.Y * Math.Tan(mult * Math.PI / 4), 0, 0);
-                _elbow = viewTransform.OfPoint(A + delta);
-                return _elbow;
-            }
+                _elbowPosition = _tagHeadPosition + delta;
         }
 
-        private XYZ _leaderStart;
-        public XYZ LeaderStart
+        private XYZ _leaderEnd;
+        public XYZ LeaderEnd
         {
-            get { return _leaderStart; }
+            get { return _leaderEnd; }
         }
 
-        private void GetTaggedElement()
+        private double _tagHeight;
+        public double TagHeight
         {
+            get { return _tagHeight; }
+        }
+
+        private double _tagWidth;
+        public double TagWidth
+        {
+            get { return _tagWidth; }
+        }
+
+        private Element GetTaggedElement()
+        {
+            Element taggedElement;
             if (_tag.TaggedElementId.HostElementId == ElementId.InvalidElementId)
             {
                 RevitLinkInstance linkInstance = _doc.GetElement(_tag.TaggedElementId.LinkInstanceId) as RevitLinkInstance;
                 Document linkedDocument = linkInstance.GetLinkDocument();
 
-                _taggedElement = linkedDocument.GetElement(_tag.TaggedElementId.LinkedElementId);
+                taggedElement = linkedDocument.GetElement(_tag.TaggedElementId.LinkedElementId);
             }
             else
             {
-                _taggedElement = _doc.GetElement(_tag.TaggedElementId.HostElementId);
+                taggedElement = _doc.GetElement(_tag.TaggedElementId.HostElementId);
             }
+
+            return taggedElement;
         }
 
-        private void GetLeaderStart()
+        private void GetTagDimension()
+        {
+            BoundingBoxXYZ bbox = _tag.get_BoundingBox(_currentView);
+            BoundingBoxXYZ viewBox = _currentView.CropBox;
+
+            _tagHeight = viewBox.Transform.Inverse.OfPoint(bbox.Max).Y - viewBox.Transform.Inverse.OfPoint(bbox.Min).Y;
+            _tagWidth = viewBox.Transform.Inverse.OfPoint(bbox.Max).X - viewBox.Transform.Inverse.OfPoint(bbox.Min).X;
+        }
+
+        private XYZ GetLeaderEnd()
         {
             BoundingBoxXYZ bbox = _taggedElement.get_BoundingBox(_currentView);
             BoundingBoxXYZ viewBox = _currentView.CropBox;
@@ -338,11 +273,17 @@ namespace AlignTag
             XYZ leaderStart = (bbox.Max + bbox.Min) / 2;
 
             //Get leader end in view reference
-            _leaderStart = viewBox.Transform.Inverse.OfPoint(leaderStart);
-            _leaderStart = new XYZ(_leaderStart.X, _leaderStart.Y, 0);
+            leaderStart = viewBox.Transform.Inverse.OfPoint(leaderStart);
+            leaderStart = new XYZ(leaderStart.X, leaderStart.Y, 0);
 
-            //Get leader end in global reference
-            _leaderStart = viewBox.Transform.OfPoint(_leaderStart);
+            return leaderStart;
+        }
+
+        public void UpdateTagPosition()
+        {
+            _tag.LeaderEndCondition = LeaderEndCondition.Attached;
+            _tag.TagHeadPosition = _currentView.CropBox.Transform.OfPoint(_tagHeadPosition);
+            _tag.LeaderElbow = _currentView.CropBox.Transform.OfPoint(_elbowPosition);
         }
     }
 }
